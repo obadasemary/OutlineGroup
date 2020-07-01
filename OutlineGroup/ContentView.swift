f82f5297d72123974243e56c45f5818099bc5c07
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-07-01 15:18:35 +0300

    Update ContentView.swift

--- OutlineGroup/ContentView.swift
+++ OutlineGroup/ContentView.swift
@@ -7,9 +7,42 @@
 
 import SwiftUI
 
+struct ListItem: Identifiable {
+
+    let id: UUID = UUID()
+    let name: String
+    var items: [ListItem]?
+}
+
+extension ListItem {
+
+    static func all() -> [ListItem] {
+
+        let animals = [ListItem(name: "🐶"), ListItem(name: "🐰")]
+        let fruits = [ListItem(name: "🍑"), ListItem(name: "🍉"), ListItem(name: "🍌")]
+        let cars = [ListItem(name: "🚗"), ListItem(name: "🏎"), ListItem(name: "🚓"), ListItem(name: "🚑"), ListItem(name: "🚜")]
+        return [
+            ListItem(name: "Animals", items: animals),
+            ListItem(name: "Fruits", items: fruits),
+            ListItem(name: "Cars", items: cars),
+        ]
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!").padding()
+        NavigationView {
+            List {
+                ForEach(ListItem.all()) { category in
+                    Section(header: EmptyView()) {
+                        OutlineGroup(category, children: \.items) { item in
+                            Text(item.name)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("OutlineGroup")
+        }
     }
 }
 
